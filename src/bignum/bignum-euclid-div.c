cb9f136d9f2d61c384c97fb37d009b0268f863b2
Bastien Morino <bastien.morino@epita.fr>
2024-02-26 15:54:24 +0100

    refactor: Rename `bignum-div.c` to `bignum-euclid-div.c`
    Simplify `remainder` function to return an i32

--- src/bignum/bignum-euclid-div.c
+++ src/bignum/bignum-euclid-div.c
@@ -1,17 +1,21 @@
+/** 
+ * Implementation of the division algorithm presented by Per Brinch Hansen,
+ * at the Syracuse University.
+ * See https://surface.syr.edu/cgi/viewcontent.cgi?article=1162&context=eecs_techreports
+ */
+
 #include "bignum-internal.h"
 #include "bignum/bignum.h"
 #include "error.h"
 #include "util.h"
 #include <stdlib.h>
 
-/*
- */
 #define RADIX 65536
 #define RADIX_POWER 16
 #define RADIX_MASK 65535
 
 static u16 getWord(const bignum* num, u64 idx) {
-    if(idx >= num->size * 2)
+    if (idx >= num->size * 2)
         return 0;
     return ((u16*)num->words)[idx];
 }
@@ -41,7 +45,7 @@ static void product(bignum* x, const bignum* y, u16 k) {
         setWord(x, i, temp & RADIX_MASK);
         carry = temp >> RADIX_POWER;
     }
-    if(carry != 0)
+    if (carry != 0)
         appendWord(x, carry);
 }
 
@@ -68,20 +72,18 @@ static inline u32 min(u32 a, u32 b) {
     return a < b ? a : b;
 }
 
-static void remainder(bignum* x, bignum* y, u32 k) {
-    bnReserve(x, 1);
+static i32 remainder(bignum* y, u32 k) {
     u32 carry = 0;
     for (u32 i = y->size; i > 0; i--) {
         carry = (y->words[i - 1] + (((u64)carry) << 32)) % k;
     }
-    x->words[0] = carry;
+    return carry;
 }
 
 static u16 trial(bignum* r, bignum* d, u32 k, u32 m) {
     u64 km = k + m;
-    u64 r3 = ((u64)getWord(r, km) << (RADIX_POWER + RADIX_POWER))
-        + (getWord(r, km - 1) << RADIX_POWER)
-        + getWord(r, km - 2);
+    u64 r3 =
+        ((u64)getWord(r, km) << (RADIX_POWER + RADIX_POWER)) + (getWord(r, km - 1) << RADIX_POWER) + getWord(r, km - 2);
     u64 d2 = ((u64)getWord(d, m - 1) << RADIX_POWER) + getWord(d, m - 2);
     return min(r3 / d2, RADIX - 1);
 }
@@ -158,7 +160,7 @@ static void longdivide(bignum* x, const bignum* y, bignum* r) {
     trim(r);
 }
 
-void bnDiv(bignum* dividend, const bignum* divisor, bignum* outRemainder) {
+void bnEuclidDiv(bignum* dividend, const bignum* divisor, bignum* outRemainder) {
     i32 offsetA;
     i32 offsetB;
     computeUpperBound(dividend, divisor, &offsetA, &offsetB);
@@ -178,7 +180,8 @@ void bnDiv(bignum* dividend, const bignum* divisor, bignum* outRemainder) {
     }
 
     if (divisor->size == 1) {
-        remainder(outRemainder, dividend, dCpy.words[0]);
+        bnInit(outRemainder);
+        bnSet(outRemainder, remainder(dividend, dCpy.words[0]));
         largeQuotient(dividend, dCpy.words[0]);
     } else if (divisor->size - offsetB > dividend->size - offsetA) {
         bnCopy(dividend, outRemainder);
@@ -187,7 +190,30 @@ void bnDiv(bignum* dividend, const bignum* divisor, bignum* outRemainder) {
         longdivide(dividend, &dCpy, outRemainder);
 
     free(dCpy.words);
-    
+
     if (negative)
         bnNeg(dividend);
+}
+
+void bnEuclidDivl(bignum* dividend, i32 divisor, i32* outRemainder) {
+    bool negative = false;
+    if (bnSign(dividend) < 0) {
+        bnNeg(dividend);
+        negative = true;
+    }
+
+    if (divisor < 0) {
+        divisor = -divisor;
+        negative = !negative;
+    }
+
+    *outRemainder = remainder(dividend, divisor);
+    largeQuotient(dividend, divisor);
+
+    if (negative)
+        bnNeg(dividend);
+}
+
+void bnDivl(bignum* divident, i32 divisor, i32* outRemainder) {
+    
 }
